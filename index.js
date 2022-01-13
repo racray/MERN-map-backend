7e915b9018b41368e4b2006dc45c86df82744a26
racray <rayedkhan007@gmail.com>
2022-01-13 22:28:15 +0530

    backend map

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+var cors = require('cors');
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
+
+dotenv.config();
+// useCreateIndex: true,
+app.use(cors());
+app.use(express.json());
+
+mongoose 
+ .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+           })   
+ .then(() => console.log("MongoDB connected!"))
+ .catch(err => console.log(err));
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
